fc44abeefaaa1ac9fda523b4ba04422d374a13d6
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-06-07 18:35:55 +0300

    Thread & Heap Dump Actions

---
+++ src/main/java/com/github/aleksikangas/visualvm/actions/HeapDumpVisualVmAction.java
@@ -0,0 +1,16 @@
+package com.github.aleksikangas.visualvm.actions;
+
+import com.github.aleksikangas.visualvm.integration.VisualVm;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public final class HeapDumpVisualVmAction extends AbstractPidAwareVisualVmAction {
+  @Override
+  public void actionPerformed(@NotNull final AnActionEvent e) {
+    getPid(e.getDataContext()).ifPresent(pid -> {
+      final var visualVmOptions = visualVmOptionsBuilder().withHeapDumpPid(pid)
+                                                          .build();
+      VisualVm.launch(visualVmOptions);
+    });
+  }
+}
---
+++ src/main/java/com/github/aleksikangas/visualvm/actions/ThreadDumpVisualVmAction.java
@@ -0,0 +1,16 @@
+package com.github.aleksikangas.visualvm.actions;
+
+import com.github.aleksikangas.visualvm.integration.VisualVm;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public final class ThreadDumpVisualVmAction extends AbstractPidAwareVisualVmAction {
+  @Override
+  public void actionPerformed(@NotNull final AnActionEvent e) {
+    getPid(e.getDataContext()).ifPresent(pid -> {
+      final var visualVmOptions = visualVmOptionsBuilder().withThreadDumpPid(pid)
+                                                          .build();
+      VisualVm.launch(visualVmOptions);
+    });
+  }
+}
