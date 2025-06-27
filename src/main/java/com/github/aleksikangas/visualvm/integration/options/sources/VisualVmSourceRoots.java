74722c44907d50b5f439907d0d202e4b7a241c03
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-06-27 20:45:26 +0300

    Improved source root detection

--- src/main/java/com/github/aleksikangas/visualvm/integration/options/sources/VisualVmSourceRoots.java
+++ src/main/java/com/github/aleksikangas/visualvm/integration/options/sources/VisualVmSourceRoots.java
@@ -1,48 +1,40 @@
 package com.github.aleksikangas.visualvm.integration.options.sources;
 
+import com.github.aleksikangas.visualvm.notifications.VisualVmNotifications;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public final class VisualVmSourceRoots {
-  private final Set<VirtualFile> sourceRoots;
+  private final Set<String> sourceRoots;
 
   public static Optional<VisualVmSourceRoots> resolve(@Nullable final Project project) {
     if (project == null) {
+      VisualVmNotifications.notifyInfo(null, "Source roots could not be resolved, since project does not exist.");
       return Optional.empty();
     }
-    final Set<VirtualFile> sourceRoots = new HashSet<>();
+    final Set<String> sourceRoots = new HashSet<>();
     final var moduleManager = ModuleManager.getInstance(project);
     for (final var module : moduleManager.getModules()) {
-      final var moduleRootManager = ModuleRootManager.getInstance(module);
-      sourceRoots.addAll(List.of(moduleRootManager.getSourceRoots(false)));
+      sourceRoots.addAll(VisualVmSourceUtils.resolveModuleSourceRoots(module));
+      sourceRoots.addAll(VisualVmSourceUtils.resolveModuleLibrarySourceRoots(module));
+      sourceRoots.addAll(VisualVmSourceUtils.resolveSdkSourceRoots(module));
     }
     return Optional.of(new VisualVmSourceRoots(sourceRoots));
   }
 
   @Override
   public String toString() {
-    final StringBuilder stringBuilder = new StringBuilder();
-    for (final var sourceRoot : sourceRoots) {
-      stringBuilder.append(sourceRoot.getPath())
-                   .append(File.pathSeparator);
-    }
-    if (!sourceRoots.isEmpty()) {
-      stringBuilder.deleteCharAt(stringBuilder.length() - 1);  // Remove the last path separator
-    }
-    return VisualVmSourceUtils.encodeValue(stringBuilder.toString());
+    return VisualVmSourceUtils.encodeValue(String.join(File.pathSeparator, sourceRoots));
   }
 
-  private VisualVmSourceRoots(@NotNull final Set<VirtualFile> sourceRoots) {
+  private VisualVmSourceRoots(@NotNull final Set<String> sourceRoots) {
     this.sourceRoots = sourceRoots;
   }
 }
--- src/main/java/com/github/aleksikangas/visualvm/integration/options/sources/VisualVmSourceUtils.java
+++ src/main/java/com/github/aleksikangas/visualvm/integration/options/sources/VisualVmSourceUtils.java
@@ -1,6 +1,17 @@
 package com.github.aleksikangas.visualvm.integration.options.sources;
 
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.SourceFolder;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public final class VisualVmSourceUtils {
   /**
@@ -17,5 +28,114 @@ public final class VisualVmSourceUtils {
     return value.replace(" ", "%20")
                 .replace("'", "%27")
                 .replace("\"", "%22");
+  }
+
+  /**
+   * Resolve the source roots for a given module.
+   * @param module the module to resolve the source roots for
+   * @return the resolved source roots
+   * @apiNote The resolved source roots follow the convention <code>root[subpaths=subpath1:subpath2]</code>.
+   */
+  public static Set<String> resolveModuleSourceRoots(@NotNull final Module module) {
+    final var moduleRootManager = ModuleRootManager.getInstance(module);
+    final var sourceRootSubPaths = new SourceRootSubPaths();
+    for (final ContentEntry contentEntry : moduleRootManager.getContentEntries()) {
+      final var contentEntryRoot = contentEntry.getFile();
+      if (contentEntryRoot == null) continue;
+      Arrays.stream(contentEntry.getSourceFolders())
+            .filter(sourceFolder -> !sourceFolder.isTestSource())
+            .map(SourceFolder::getFile)
+            .filter(Objects::nonNull)
+            .forEach(sourceRootPath -> {
+              sourceRootSubPaths.add(contentEntryRoot.getPath(),
+                                     VfsUtilCore.getRelativePath(sourceRootPath, contentEntryRoot));
+            });
+    }
+    return sourceRootSubPaths.combine();
+  }
+
+  /**
+   * Resolve the library source roots for a given module.
+   * @param module the module to resolve the source roots for
+   * @return the resolved source roots
+   * @apiNote The resolved source roots follow the convention <code>root[subpaths=subpath1:subpath2]</code>.
+   */
+  public static Set<String> resolveModuleLibrarySourceRoots(@NotNull final Module module) {
+    final var moduleRootManager = ModuleRootManager.getInstance(module);
+    final var librarySubPaths = resolveArchiveSourceRootSubPaths(moduleRootManager.orderEntries()
+                                                                                  .librariesOnly()
+                                                                                  .sources()
+                                                                                  .usingCache()
+                                                                                  .getRoots());
+    return librarySubPaths.combine();
+  }
+
+  /**
+   * Resolve the SDK source roots for a given module.
+   * @param module the module to resolve the source roots for
+   * @return the resolved source roots
+   * @apiNote The resolved source roots follow the convention <code>root[subpaths=subpath1:subpath2]</code>.
+   */
+  public static Set<String> resolveSdkSourceRoots(@NotNull final Module module) {
+    final var moduleRootManager = ModuleRootManager.getInstance(module);
+    final var sdkSubPaths = resolveArchiveSourceRootSubPaths(moduleRootManager.orderEntries()
+                                                                              .sdkOnly()
+                                                                              .sources()
+                                                                              .usingCache()
+                                                                              .getRoots());
+    return sdkSubPaths.combine();
+  }
+
+  private static SourceRootSubPaths resolveArchiveSourceRootSubPaths(@NotNull final VirtualFile[] sourceRoots) {
+    final var ARCHIVE_SUBPATH_SEPARATOR = "!/";
+    final var archiveSubPaths = new SourceRootSubPaths();
+    Arrays.stream(sourceRoots)
+          .map(VirtualFile::getPath)
+          .forEach(sourceRootPath -> {
+            if (sourceRootPath.contains(ARCHIVE_SUBPATH_SEPARATOR)) {
+              final var archive = StringUtil.substringBefore(sourceRootPath, ARCHIVE_SUBPATH_SEPARATOR);
+              final var subPath = StringUtil.substringAfter(sourceRootPath, ARCHIVE_SUBPATH_SEPARATOR);
+              archiveSubPaths.add(archive, subPath);
+            } else {
+              archiveSubPaths.add(sourceRootPath);
+            }
+          });
+    return archiveSubPaths;
+  }
+
+  private static final class SourceRootSubPaths {
+    private final Map<String, Set<String>> sourceRootSubPaths = new HashMap<>();
+
+    public void add(@Nullable final String sourceRoot) {
+      if (sourceRoot == null) return;
+      sourceRootSubPaths.computeIfAbsent(sourceRoot, k -> new HashSet<>());
+    }
+
+    public void add(@Nullable final String sourceRoot, @Nullable final String subPath) {
+      if (sourceRoot == null) return;
+      if (subPath == null || subPath.isBlank()) {
+        sourceRootSubPaths.computeIfAbsent(sourceRoot, k -> new HashSet<>());
+      } else {
+        sourceRootSubPaths.computeIfAbsent(sourceRoot, k -> new HashSet<>()).add(subPath);
+      }
+    }
+
+    public Set<String> combine() {
+      return sourceRootSubPaths.entrySet()
+                               .stream()
+                               .map(e -> {
+                                 final var sourceRoot = e.getKey();
+                                 final var subPaths = e.getValue();
+                                 if (subPaths.isEmpty()) {
+                                   return sourceRoot;
+                                 } else {
+                                   return String.format("%s[subpaths=%s]", sourceRoot, String.join(":", subPaths));
+                                 }
+                               })
+                               .collect(Collectors.toSet());
+    }
+  }
+
+  private VisualVmSourceUtils() {
   }
 }
